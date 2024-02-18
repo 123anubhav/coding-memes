bca18da4ec3cded7e1ed4065784288cd25ffd485
Anubhav Chaudhary <31804245+123anubhav@users.noreply.github.com>
2024-02-18 09:11:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const meme_url = "https://meme-api.com/gimme/codingmemes";
+
+const generateMemeBtn = document.getElementById("button");
+const memeImage = document.querySelector(".container img");
+const memeTitle = document.querySelector(".container .meme-title")
+
+const Details = (url, title) => {
+    memeImage.setAttribute("src", url)
+    memeTitle.innerHTML = title;
+}
+
+const generateMeme = () => {
+    fetch(meme_url)
+        .then((response) => response.json())
+        .then((data) => {
+            Details(data.url, data.title)
+        });
+};
+
+generateMemeBtn.addEventListener("click", generateMeme);
+
+generateMeme();
